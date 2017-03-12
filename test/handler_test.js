aeb337bf392c6fe710dfa2347786647392afb350
Chris Dawson <xrdawson@gmail.com>
2017-03-12 16:49:10 -0700

    Start testing ask function

--- test/handler_test.js
+++ test/handler_test.js
@@ -1,14 +1,29 @@
 var test = require('assert');
 
-describe('Handler', function() {
+describe( 'Handler', function() {
     var h = require( '../handler.js' )
     // always return 3
     h.setRandomNumberGenerator( function() { return 3 } );
+
+    var assistant;
+    
+    beforeEach( function() {
+	assistant = { data: {}, ask: function() { /* console.log( "Called ask" ); */ } }
+	// spy on the ask function next... NYI.
+    });
     
     it('should test the generateAnswer function', function() {
-	let assistant = { data: {}, ask: function() { console.log( "Called ask" ); } }
 	ans = h.generateAnswer( assistant )
 	test.equal( assistant.data.answer, 3 );
+	test.equal( assistant.data.guessCount, 0 );
+	test.equal( assistant.data.fallbackCount, 0 );
+	test.equal( assistant.data.steamSoundCount, 0 );
     })
 
+    it( "should test the ask function", function() {
+	h.ask( assistant, "some prompt", undefined );
+	assistant.data.printed = "hi there";
+	test.equal( assistant.data.lastPrompt, assistant.data.printed );
+    });
+    
 });
