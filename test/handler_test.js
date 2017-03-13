653ad9262bdf8ca262d6b952eb7c1231168cadad
Chris Dawson <xrdawson@gmail.com>
2017-03-13 14:32:29 -0700

    Use sinon for spies

--- test/handler_test.js
+++ test/handler_test.js
@@ -1,4 +1,5 @@
-var test = require('assert');
+var test = require( 'assert' );
+var sinon = require( 'sinon' );
 
 describe( 'Handler', function() {
     var h = require( '../handler.js' )
@@ -6,24 +7,46 @@ describe( 'Handler', function() {
     h.setRandomNumberGenerator( function() { return 3 } );
 
     var assistant;
+    var askSpy;
+    var ask = function() {}
     
     beforeEach( function() {
-	assistant = { data: {}, ask: function() { /* console.log( "Called ask" ); */ } }
-	// spy on the ask function next... NYI.
+	//askSpy = chai.spy(ask);
+	assistant = { data: {}, ask: ask }
     });
     
-    it('should test the generateAnswer function', function() {
+    it('should test the generateAnswer function with default values', function() {
 	ans = h.generateAnswer( assistant )
 	test.equal( assistant.data.answer, 3 );
 	test.equal( assistant.data.guessCount, 0 );
 	test.equal( assistant.data.fallbackCount, 0 );
 	test.equal( assistant.data.steamSoundCount, 0 );
+	// test.equal( askSpy.should.have.been.called(), true )
+	// expect(askSpy.Spy).should.have.been.called()
     })
 
     it( "should test the ask function", function() {
 	assistant.data.printed = "hi there";
 	h.ask( assistant, "some prompt" )
 	test.equal( assistant.data.lastPrompt, assistant.data.printed );
+	// console.log( "askSpy", askSpy )
+	// expect(askSpy).should.have.been.called()
+    });
+
+    it( "should test spies", function() {
+
+	function original () {
+	    // do something cool
+	    console.log( "Ummmm..." );
+	}
+
+	var spyIt = sinon.spy( original );
+	// console.log( "Spy", require('util').inspect( spyIt ) );
+	// console.log( "Func", function() { } );
+	// original();
+	spyIt();
+	test.equal( spyIt.calledOnce, true );
+	// expect(spyIt).should.have.been.called();
     });
     
 });
